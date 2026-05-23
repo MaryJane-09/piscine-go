5dc8474ed179023e5fbc2cb0b0b08abbb72710f9
MaryJane-09 <nzekwechinaza0613@gmail.com>
2026-05-23 10:06:27 +0100

    wrote another approach to printdigit function

--- printdigits/main.go
+++ printdigits/main.go
@@ -2,9 +2,16 @@ package main
 
 import "github.com/01-edu/z01"
 
+// func main() {
+// 	alpha := "0123456789"
+// 	for _, i := range alpha {
+// 		z01.PrintRune(i)
+// 	}
+// 	z01.PrintRune('\n')
+// }
+
 func main() {
-	alpha := "0123456789"
-	for _, i := range alpha {
+	for i := '0'; i <= '9'; i++ {
 		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')
